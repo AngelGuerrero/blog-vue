93c366b291538fdcc5d26f6d9d39ae3d2ab2aa10
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2019-01-05 03:35:33 -0600

    CAMBIANDO LA RUTA PARA LOS MODELOS DE LA BD

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,54 +1,85 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../db/models/index');
+var {
+  response
+} = require('../lib/common')
+
+// Models
 var Post = models.Post;
 var Tag = models.Tag;
 var User = models.User;
 
 /* GET all posts */
 router.get('/', (req, res, next) => {
-  Post.findAll({
-    include: [{ model: User },
-              { model: Tag, as: 'tags' }]
-  }).then(posts => res.json(posts));
+  let query = {
+    include: [{
+        model: User
+      },
+      {
+        model: Tag,
+        as: 'tags'
+      }
+    ]
+  };
+
+  Post.findAll(query)
+    .then(posts => response(posts, res))
+    .catch(err => res.status(500).json(err));
 });
 
 /* GET a specific post */
 router.get('/:id', (req, res, next) => {
-  Post.findAll({
-      where: { id: req.params.id },
-      include: [{ model: User },
-                { model: Tag, as: 'tags' }]
-    })
-    .then(post => res.json(post))
-})
+  let query = {
+    where: {
+      id: req.params.id
+    },
+    include: [{
+        model: User
+      },
+      {
+        model: Tag,
+        as: 'tags'
+      }
+    ]
+  };
+
+  Post.findAll(query)
+    .then(post => response(post, res))
+    .catch(err => res.status(500).json(err));
+});
 
 /* POST create a new post */
 router.post('/', (req, res, next) => {
-  Post.create(req.body).then(post => res.json(post))
+  Post.create(req.body)
+    .then(post => res.json(post))
+    .catch(err => res.status(500).json(err));
 });
 
 /* PUT edit a existing post */
 router.put('/:id', (req, res, next) => {
-  Post.update(req.body, {
-    where: { id: req.params.id }
-  })
-  .then(affectedRows => {
-    if(affectedRows == 0) return res.status(404).json({ message: "Resource not found" })
-    
-    res.status(200).json({ affectedRows })
-  })
-})
+  let query = {
+    where: {
+      id: req.params.id
+    }
+  };
+
+  Post.update(req.body, query)
+    .then(affectedRows => response(affectedRows, res))
+    .catch(err => res.status(500).json(err));
+});
 
 /* DELETE an existing post */
 router.delete('/:id', (req, res, next) => {
-  Post.destroy({
-    where: { id: req.params.id }
-  })
-  .then(affectedRows => {
-    if(affectedRows == 0) return res.status(404).json({ message: "Resource not found" })
-    
-    res.status(200).json({ affectedRows })
-  })
-})
+  let query = {
+    where: {
+      id: req.params.id
+    }
+  };
+
+  Post.destroy(query)
+    .then(affectedRows => response(affectedRows, res))
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
